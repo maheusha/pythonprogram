98d6945f5d4e149d04cdfaf7cda722105bf7fd7c
maheusha <42572933+maheusha@users.noreply.github.com>
2018-09-01 21:01:41 +0530

    Create odd number.py

---
+++ odd number.py
@@ -0,0 +1,7 @@
+alp=(raw_input())
+if(alp in 'a','e','i','o','u','A','E','I','O','U'):
+    print("vowel")
+elif(alp in 'q','w','r','t','y','p','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','R','T','Y','P','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M',):
+    print("consonent")
+else:
+          print("invalid")
