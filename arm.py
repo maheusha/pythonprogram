73b20cf0d7ac2fc29bb513a09a470a56fb093b20
maheusha <42572933+maheusha@users.noreply.github.com>
2018-11-24 12:02:57 +0530

    Create arm.py

---
+++ arm.py
@@ -0,0 +1,10 @@
+for num in range(150,160):
+    order=len(str(num))
+    temp=num
+    sum=0
+    while temp>0:
+        digit=temp%10
+        sum +=digit**order
+        temp//=10
+    if num==sum:
+        print(num)
