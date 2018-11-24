180d17166c99557bcc2c9c05ebfeac1e1b9e9e9a
maheusha <42572933+maheusha@users.noreply.github.com>
2018-11-24 11:57:56 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+num=(raw_input())
+order=len(str(num))
+temp=num
+sum=0
+while temp>0:
+    digit=temp%10
+    sum +=digit**order
+    temp//=10
+if num==sum:
+    print("yes")
+else:
+    print("no")
