7d2dfedd3823174766d99bd54620870eb5736c97
maheusha <42572933+maheusha@users.noreply.github.com>
2018-12-10 12:13:38 +0530

    Create 1 to one.py

---
+++ 1 to one.py
@@ -0,0 +1,23 @@
+N=int(raw_input())
+1<=N<=10
+if(N==1):
+    print("One")
+elif(N==2):
+    print("Two")
+elif(N==3):
+    print("Three")
+elif(N==4):
+    print("Four")
+elif(N==5):
+    print("Five")
+elif(N==6):
+    print("Six")
+elif(N==7):
+    print("Seven")
+elif(N==8):
+    print("Eight")
+elif(N==9):
+    print("Nine")
+elif(N==10):
+    print("Ten")
+
