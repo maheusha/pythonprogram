5d68bbc5881b51924f047af88d1e7f58afcf5c11
maheusha <42572933+maheusha@users.noreply.github.com>
2018-12-18 20:08:00 +0530

    Update reverse a list.py

--- reverse a list.py
+++ reverse a list.py
@@ -1,7 +1,7 @@
-N= int(input())
-L= [ int(x) for x in input().split()]
+N= int(raw_input())
+L= [ int(x) for x in raw_input().split()]
 R= L[::-1]
 p= len(R)
 for i in range(0,p-1) :
-    print(R[i],end='->')
+    print(R[i],end="->")
 print(R[p-1])
