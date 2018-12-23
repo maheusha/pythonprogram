f10602f5ed7b282be5256577e097cd28d8cfd4da
maheusha <42572933+maheusha@users.noreply.github.com>
2018-12-23 06:37:52 +0530

    Create String constists of '('and')'.py

---
+++ String constists of '('and')'.py
@@ -0,0 +1,12 @@
+S=str(raw_input())
+c=0
+for x in range(0,len(S)):
+    if(S[x]=='('):
+        c=c+1
+    if(S[x]==')'):
+        c=c-1
+if(c==1):
+  print("yes")
+else:
+    print("no")
+        
