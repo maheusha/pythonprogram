f7707ddb5b82aad4caa9c695a056719893716f2b
maheusha <42572933+maheusha@users.noreply.github.com>
2018-12-25 09:29:59 +0530

    Update longest common prefix.py

--- longest common prefix.py
+++ longest common prefix.py
@@ -1,9 +1,9 @@
 i=int(input())
 s=input()
-s1=input()
+p1=input()
 s2=[]
 for i in range(0,len(s)):
-    if s[i]==s1[i]:
+    if s[i]==p1[i]:
         s2.append(s[i])
 s=''.join(s2)        
 print(s)
